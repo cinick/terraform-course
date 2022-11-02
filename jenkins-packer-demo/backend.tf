2a623c353d12b926c4560b496417651bb103169a
cinick <vitaliy.kizhlo@gmail.com>
2022-11-02 19:09:01 +0200

    Update backend.tf

--- jenkins-packer-demo/backend.tf
+++ jenkins-packer-demo/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-fx3a"
-    key    = "terraform.tfstate"
-    region = "eu-west-2"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-state-fx3a"
+#    key    = "terraform.tfstate"
+#    region = "eu-west-2"
+#  }
+#}
